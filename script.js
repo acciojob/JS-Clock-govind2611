eefeec3d5bbd27145c5b41e111602ad7531f815e
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-03-30 10:12:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-//your code here
+// Get the clock elements
+const hourHand = document.querySelector('.hour-hand');
+const minuteHand = document.querySelector('.minute-hand');
+const secondHand = document.querySelector('.second-hand');
+
+// Function to update the clock hands
+function updateClock() {
+  // Get the current time
+  const now = new Date();
+
+  // Get the hours, minutes and seconds
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+  const seconds = now.getSeconds();
+
+  // Calculate the angles for the clock hands
+  const hourAngle = (hours / 12) * 360 + (minutes / 60) * 30;
+  const minuteAngle = (minutes / 60) * 360;
+  const secondAngle = (seconds / 60) * 360;
+
+  // Rotate the clock hands
+  hourHand.style.transform = `rotate(${hourAngle}deg)`;
+  minuteHand.style.transform = `rotate(${minuteAngle}deg)`;
+  secondHand.style.transform = `rotate(${secondAngle}deg)`;
+}
+
+// Update the clock every second
+setInterval(updateClock, 1000);
