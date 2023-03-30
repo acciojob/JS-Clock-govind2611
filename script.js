bac0ab69e6e134bf3d58e36151416a0a0f46ac0e
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-03-30 10:13:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,24 @@
-// Get the clock elements
-const hourHand = document.querySelector('.hour-hand');
-const minuteHand = document.querySelector('.minute-hand');
-const secondHand = document.querySelector('.second-hand');
+const secondHand = document.querySelector(".second-hand");
+const minuteHand = document.querySelector(".min-hand");
+const hourHand = document.querySelector(".hour-hand");
 
-// Function to update the clock hands
-function updateClock() {
-  // Get the current time
+function setDate() {
   const now = new Date();
 
-  // Get the hours, minutes and seconds
-  const hours = now.getHours();
-  const minutes = now.getMinutes();
+  // Seconds hand
   const seconds = now.getSeconds();
+  const secondsDegrees = (seconds / 60) * 360 + 90;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
-  // Calculate the angles for the clock hands
-  const hourAngle = (hours / 12) * 360 + (minutes / 60) * 30;
-  const minuteAngle = (minutes / 60) * 360;
-  const secondAngle = (seconds / 60) * 360;
+  // Minutes hand
+  const minutes = now.getMinutes();
+  const minutesDegrees = (minutes / 60) * 360 + (seconds / 60) * 6 + 90;
+  minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
 
-  // Rotate the clock hands
-  hourHand.style.transform = `rotate(${hourAngle}deg)`;
-  minuteHand.style.transform = `rotate(${minuteAngle}deg)`;
-  secondHand.style.transform = `rotate(${secondAngle}deg)`;
+  // Hours hand
+  const hours = now.getHours();
+  const hoursDegrees = (hours / 12) * 360 + (minutes / 60) * 30 + 90;
+  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
 }
 
-// Update the clock every second
-setInterval(updateClock, 1000);
+setInterval(setDate, 1000);
